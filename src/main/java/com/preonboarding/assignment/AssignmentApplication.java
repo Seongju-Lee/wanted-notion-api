6a7d19ccfb2f6f7d2d1ba28d5820bafe97982416
Seongju <lsj40413@gmail.com>
2023-09-04 04:18:11 +0900

    test: PageRepository Layer
    - DB 연결시, 테이블 생성 및 데이터 삽입을 위한 DatabaseInitializr 구현

--- src/main/java/com/preonboarding/assignment/AssignmentApplication.java
+++ src/main/java/com/preonboarding/assignment/AssignmentApplication.java
@@ -2,12 +2,19 @@ package com.preonboarding.assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignmentApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(
+			AssignmentApplication.class,
+			args);
+
+		DatabaseInitializr initializationService = context.getBean(
+			DatabaseInitializr.class);
+		initializationService.initializeDatabase();
 	}
 
 }
